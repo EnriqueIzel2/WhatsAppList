6a7f887560e7c006dbe33b84065f5832a51371a7
EnriqueIzel2 <eleaoizel@gmail.com>
2022-07-04 10:50:20 -0400

    feat: set the layout manager

--- app/src/main/java/com/enriqueizel/whatsapplist/MainActivity.java
+++ app/src/main/java/com/enriqueizel/whatsapplist/MainActivity.java
@@ -1,19 +1,26 @@
 package com.enriqueizel.whatsapplist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-  private RecyclerView recyclerUser;
+  private RecyclerView recyclerUsers;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    recyclerUser = findViewById(R.id.recycler_users);
+    recyclerUsers = findViewById(R.id.recycler_users);
+
+    recyclerUsers.setLayoutManager(new LinearLayoutManager(
+            MainActivity.this,
+            LinearLayoutManager.VERTICAL,
+            false));
+    recyclerUsers.setHasFixedSize(true);
   }
 }
