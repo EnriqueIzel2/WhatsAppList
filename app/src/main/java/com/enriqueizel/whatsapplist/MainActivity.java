0c869cfda2890e65450f896e62cecbffe6550def
EnriqueIzel2 <eleaoizel@gmail.com>
2022-07-04 18:17:51 -0400

    feat: create user list to show

--- app/src/main/java/com/enriqueizel/whatsapplist/MainActivity.java
+++ app/src/main/java/com/enriqueizel/whatsapplist/MainActivity.java
@@ -6,9 +6,16 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.enriqueizel.whatsapplist.Adapter.AdapterUser;
+import com.enriqueizel.whatsapplist.Model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
   private RecyclerView recyclerUsers;
+  private List<User> userList = new ArrayList<>();
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +29,41 @@ public class MainActivity extends AppCompatActivity {
             LinearLayoutManager.VERTICAL,
             false));
     recyclerUsers.setHasFixedSize(true);
+    AdapterUser adapterUser = new AdapterUser(userList);
+    recyclerUsers.setAdapter(adapterUser);
+
+    generateUsers();
+  }
+
+  public void generateUsers() {
+    User user1 = new User(R.drawable.usuario1, "John", "Suave??");
+    userList.add(user1);
+
+    User user2 = new User(R.drawable.usuario2, "Natan", "Hoje tem futebol");
+    userList.add(user2);
+
+    User user3 = new User(R.drawable.usuario1, "John", "Bom dia");
+    userList.add(user3);
+
+    User user4 = new User(R.drawable.usuario2, "Jo", "Como assim");
+    userList.add(user4);
+
+    User user5 = new User(R.drawable.usuario1, "Willian", "olar");
+    userList.add(user5);
+
+    User user6 = new User(R.drawable.usuario2, "Taxa", "Suave??");
+    userList.add(user6);
+
+    User user7 = new User(R.drawable.usuario1, "Ferdiand", "Suave??");
+    userList.add(user7);
+
+    User user8 = new User(R.drawable.usuario2, "Rwan", "Suave??");
+    userList.add(user8);
+
+    User user9 = new User(R.drawable.usuario1, "Antonio", "Suave??");
+    userList.add(user9);
+
+    User user10 = new User(R.drawable.usuario2, "Amora", "Suave??");
+    userList.add(user10);
   }
 }
